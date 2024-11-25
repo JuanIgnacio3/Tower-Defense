5e1bedc9f670cab9e8afba24d950946d10c1fd5f
valverdegit <157626638+valverdegit@users.noreply.github.com>
2024-11-25 14:04:22 -0600

    Update Usuario.java

--- ProyectoTowerDefense/src/main/java/steven_progra/proyectotowerdefense/Usuario.java
+++ ProyectoTowerDefense/src/main/java/steven_progra/proyectotowerdefense/Usuario.java
@@ -1,12 +1,13 @@
 package steven_progra.proyectotowerdefense;
-
 class Usuario {
     private String nombre;
     private boolean ganoBatalla;
+    private Castillo castillo; // Castillo asociado al jugador
 
     // Constructor
-    public Usuario(String nombre) {
+    public Usuario(String nombre, Castillo castillo) {
         this.nombre = nombre;
+        this.castillo = castillo;
         this.ganoBatalla = false; // Por defecto no ha ganado ninguna batalla
     }
 
@@ -26,5 +27,12 @@ class Usuario {
     public void setGanoBatalla(boolean ganoBatalla) {
         this.ganoBatalla = ganoBatalla;
     }
-}
 
+    public Castillo getCastillo() {
+        return castillo;
+    }
+
+    public void setCastillo(Castillo castillo) {
+        this.castillo = castillo;
+    }
+}
