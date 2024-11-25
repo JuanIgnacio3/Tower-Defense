b9b5e0f5d673597a5939909bbfae47a0c155ff99
valverdegit <157626638+valverdegit@users.noreply.github.com>
2024-11-25 13:53:58 -0600

    Update Cpu.java

--- ProyectoTowerDefense/src/main/java/steven_progra/proyectotowerdefense/Cpu.java
+++ ProyectoTowerDefense/src/main/java/steven_progra/proyectotowerdefense/Cpu.java
@@ -1,1 +1,29 @@
 package steven_progra.proyectotowerdefense;
+
+class CPU {
+    private String nombre;
+    private boolean ganoBatalla;
+
+    // Constructor
+    public CPU(String nombre) {
+        this.nombre = nombre;
+        this.ganoBatalla = false; // Por defecto no ha ganado ninguna batalla
+    }
+
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isGanoBatalla() {
+        return ganoBatalla;
+    }
+
+    public void setGanoBatalla(boolean ganoBatalla) {
+        this.ganoBatalla = ganoBatalla;
+    }
+}
