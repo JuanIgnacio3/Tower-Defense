ee7c45866e07c208287e8f1bf82fa499afbfa0b1
valverdegit <157626638+valverdegit@users.noreply.github.com>
2024-11-25 18:06:10 -0600

    Create Nodo.java

---
+++ ProyectoTowerDefense/src/main/java/steven_progra/proyectotowerdefense/Nodo.java
@@ -0,0 +1,26 @@
+package steven_progra.proyectotowerdefense;
+class Nodo {
+    private Object dato;
+    private Nodo siguiente;
+
+    public Nodo(Object dato) {
+        this.dato = dato;
+        this.siguiente = null;
+    }
+
+    public Object getDato() {
+        return dato;
+    }
+
+    public void setDato(Object dato) {
+        this.dato = dato;
+    }
+
+    public Nodo getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+}
